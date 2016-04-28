6094775de5db3bf70089f4bb122a8561c4bee1ef
ReneeVL85 <renee.v.leijen@gmail.com>
2016-04-28 14:27:49 +0200

    whatever

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,10 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
+
+    respond_tp do |f|
+      f.html
+      f.json {render json: {categories: @categories}}
+    end
   end
 end
